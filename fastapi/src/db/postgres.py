af4acf684fa2c59d21249eade1709a54487410e2
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-01-21 13:44:28 +0600

    Add config

--- fastapi/src/db/postgres.py
+++ fastapi/src/db/postgres.py
@@ -1,3 +1,19 @@
-"""
-Модуль подключения к базе
-"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from core.config import settings
+
+db_uri = (
+    "postgresql://"
+    f"{settings.postgres.user}:{settings.postgres.password}@"
+    f"{settings.postgres.host}:{settings.postgres.port}/"
+    f"{settings.postgres.dbname}"
+)
+db = create_engine(db_uri, convert_unicode=True, pool_size=20, max_overflow=0)
+Session = sessionmaker(bind=db, autocommit=False, autoflush=False)
+db_session = scoped_session(Session)
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
