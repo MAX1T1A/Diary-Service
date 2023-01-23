df8406743c035e8d83726a66b5091fb71a7fd92c
Aldan <max1t1aldan@gmail.com>
2023-01-23 16:12:17 +0300

    create pydantic models from user

--- fastapi/src/models/schemas.py
+++ fastapi/src/models/schemas.py
@@ -3,48 +3,12 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class PageBaseSchemas(BaseModel):
-    diary_id: int
-    name: str
-    body: str
-
-
-# class PageSchemas(PageBaseSchemas):
-#     class Config:
-#         orm_mode = True
-
-
-class DiarySchemas(BaseModel):
-    user_id: int
-    name: str
-
-
-# class ShowDiarySchemas(BaseModel):
-#     name: str
-#     page: Optional[PageSchemas]
-
-
 class UserSchemas(BaseModel):
     email: str
-    name: str
+    login: str
     password: str
 
 
-# class ShowUserSchemas(BaseModel):
-#     name: str
-#     email: str
-#     diary: Optional[DiarySchemas]
-#
-#     class Config:
-#         orm_mode = True
-
-
-# class ShowPageSchemas(BaseModel):
-#     name: str
-#     body: str
-#     creator: ShowUserSchemas
-
-
-# class LoginSchemas(BaseModel):
-#     name: str
-#     password: str
+class LoginSchemas(BaseModel):
+    login: str
+    password: str
