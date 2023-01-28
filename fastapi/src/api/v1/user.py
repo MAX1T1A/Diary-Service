8239583ed8fc222653f469d3173704a1646ff053
max1t1aldan <113845928+MAX1T1A@users.noreply.github.com>
2023-01-28 11:21:52 +0300

    Update user.py

--- fastapi/src/api/v1/user.py
+++ fastapi/src/api/v1/user.py
@@ -1,3 +1,12 @@
-"""
-Тут будет апи
-"""
+from fastapi import APIRouter, Depends
+from db.postgres import get_db
+from sqlalchemy.orm import Session
+from models.schemas import UserIn
+from repository.user import create
+
+router = APIRouter(prefix="/register", tags=['Auth'])
+
+
+@router.post('/')
+def create_user(request: UserIn, db: Session = Depends(get_db)):
+    return create(request, db)
