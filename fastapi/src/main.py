fe3983511c67f122eb2277d97a11cf0b173b8b48
max1t1aldan <113845928+MAX1T1A@users.noreply.github.com>
2023-01-28 11:19:29 +0300

    Update main.py

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -1,3 +1,10 @@
-"""
-Это базовый файл, отсюда будет запуск проекта
-"""
+from fastapi import FastAPI
+from api.v1 import user
+import uvicorn
+
+app = FastAPI(title="Diary API")
+app.include_router(user.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="127.0.0.1", reload=True)
