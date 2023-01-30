690f0675bff85d816994b09daaad355eca37a342
Vladimir <v.gorobets77@mail.ru>
2023-01-30 12:27:03 +0300

    получение ID из токена

---
+++ fastapi/src/api/v1/utils/create_token.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from core.config import settings
+
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.jwt_token.active_time)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.jwt_token.secret_key, settings.jwt_token.algorithm)
+    return encoded_jwt
