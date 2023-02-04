17bb53d61a0596a055c2219062052a71acc19e1b
Vladimir <v.gorobets77@mail.ru>
2023-02-04 13:26:36 +0300

    замечания исправлены. без красоты с префиксами пока

--- fastapi/src/api/v1/diary_routes.py
+++ fastapi/src/api/v1/diary_routes.py
@@ -6,17 +6,15 @@ from sqlalchemy.orm import Session
 from api.v1.utils.auth_bearer import JWTBearer
 from db.postgres import get_db
 from diary import schemas, service
-from models.models import User
+from models.models import Diary, Page, User
 
 router = APIRouter()
 
 
 @router.post("/diary", response_model=schemas.DiaryCreate)
-async def create_diary(
-        item: schemas.DiaryCreate,
-        db: Session = Depends(get_db),
-        user_id: User = Depends(JWTBearer())):
-    return service.create_diary(db=db, item=item, user_id=user_id)
+async def create_diary(item: schemas.DiaryCreate, db: Session = Depends(get_db), user_id: User = Depends(JWTBearer())):
+    db_item = Diary(**item.dict(), user_id=user_id)
+    return service.create_items(db=db, db_item=db_item)
 
 
 @router.get("/diary", response_model=List[schemas.DiaryGet])
@@ -24,6 +22,12 @@ async def get_diary_list(db: Session = Depends(get_db), user_id: User = Depends(
     return service.get_diary(db=db, user_id=user_id)
 
 
-@router.post("/diary", response_model=schemas.PageCreate)
-def create_page(item: schemas.PageCreate, db: Session = Depends(get_db)):
-    return service.create_page(db=db, item=item)
+@router.post("/page", response_model=schemas.PageCreate)
+async def create_page(item: schemas.PageCreate, db: Session = Depends(get_db)):
+    db_item = Page(**item.dict())
+    return service.create_items(db=db, db_item=db_item)
+
+
+# @router.get("/page", response_model=List[schemas.PageGet])
+# def get_page(diary_id: Page, db: Session = Depends(get_db)):
+#     return service.get_page(db=db, diary_id=diary_id)
