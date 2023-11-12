3fe19041954f9e6e2debc7fc20c6a0d70c9e3610
max1t1a <max1t1aldan@gmail.com>
2023-11-12 14:12:23 +0300

    fix: in working order

--- diary/src/core/config.py
+++ diary/src/core/config.py
@@ -1,6 +1,6 @@
 import os
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ENV_DIR = os.path.join(BASE_DIR, "..", "..")
